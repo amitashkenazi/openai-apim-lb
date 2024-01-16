580e5b67f6d221f0c7814bce4539ad910c7ceac1
Your Name <amit>
2024-01-16 17:43:29 +0200

    clean up

--- priority_handler.py
+++ priority_handler.py
@@ -19,15 +19,13 @@ api_url = os.environ.get('API_URL', 'https://apim-openai-lb.azure-api.net/set_be
 
 # Function for Changing Priority
 def change_priority(url, priorities):
-    print("change priority")
-    print(priorities)
     payload = json.dumps(priorities)
     headers = {
         'Ocp-Apim-Subscription-Key': api_subscription_key,
         'Content-Type': 'application/json'
     }
     response = requests.request("POST", url, headers=headers, data=payload)
-    print(response.text)
+    logging.info(response.text)
 
 # Function for Parsing Datetime Strings
 def parse_datetime(dt_str):
@@ -45,10 +43,8 @@ def get_backends(client):
     for item in container_client.query_items(
             query="SELECT * FROM c",
             enable_cross_partition_query=True):
-        print(item)
         if item.get('backends'):
             backends+=item['backends']
-    print(f"backends={backends}")
     return backends
 
 def get_docs_from_cosmos(client, query):
@@ -79,10 +75,8 @@ def set_priorities(window_size_seconds=60, priority_step=50):
     query = f"SELECT * FROM c WHERE c._ts > {ts_th}"
     performance = {}
     items = get_docs_from_cosmos(container_client, query)
-    print(f"items={items}")
     # Execute the query
     for idx, item in enumerate(items):
-        print(f"---item {idx}---")
         start_time_str = item.get("StartTime")
         end_time_str = item.get("EndTime")
         CompletionTokens = int(item.get("CompletionTokens"))
@@ -93,13 +87,6 @@ def set_priorities(window_size_seconds=60, priority_step=50):
 
             response_time_ms = (end_time - start_time).total_seconds() * 1000
             token_response_time_ratio = response_time_ms / CompletionTokens
-            print (f"response_time_ms={response_time_ms}")
-            print(f"CompletionTokens={CompletionTokens}")
-            print(f"token_response_time_ratio={token_response_time_ratio}")
-            print(item['_ts'])
-            print(int(time.time()))
-            print(f"time since doc creation={int(time.time()) - item['_ts']}")
-
             if backendUrl not in performance:
                 performance[backendUrl] = []
             performance[backendUrl].append(token_response_time_ratio)
@@ -114,8 +101,6 @@ def set_priorities(window_size_seconds=60, priority_step=50):
             response_parameter[k] = []
         calls_number[k].append(len(performance[k]))
         response_parameter[k].append(v)
-    print(f"response_parameter={response_parameter}")
-    print(f"calls_number={calls_number}")
     change_priority_dict = {}
     sorted_names = sorted(performance_avg, key=performance_avg.get)
     # set priority based on performance. if the next performance value is bigger than previous value + priority_step, then set the priority to previous value + 1
@@ -123,9 +108,6 @@ def set_priorities(window_size_seconds=60, priority_step=50):
         if idx == 0:
             change_priority_dict[n[:-7]] = 0
         else:
-            print(f"performance_avg[n]={performance_avg[n]}")
-            print(f"performance_avg[sorted_names[idx-1]]={performance_avg[sorted_names[idx-1]]}")
-
             if performance_avg[n] > performance_avg[sorted_names[idx-1]] + priority_step:
                 change_priority_dict[n[:-7]] = change_priority_dict[sorted_names[idx-1][:-7]] + 1
             else:
@@ -133,8 +115,12 @@ def set_priorities(window_size_seconds=60, priority_step=50):
 
     # check which backend is missing in the priority list and count the last time it was called from cosmos db
     for backend in backends:
+        logging.info(f"Checking backend {backend}")
     # Make sure 'backend' is a valid string for use in the query
         if not any(backend.startswith(prefix) for prefix in change_priority_dict):
+            change_priority_dict[backend+"/openai"] = idx + 1
+            calls_number[backend+"/openai"] = [0]
+            response_parameter[backend+"/openai"] = [0]
             try:
                 query = f"SELECT TOP 1 * FROM c WHERE STARTSWITH(c.backendUrl, '{backend}') ORDER BY c._ts DESC"
                 items = list(container_client.query_items(
@@ -145,14 +131,14 @@ def set_priorities(window_size_seconds=60, priority_step=50):
                 if items:
                     item = items[0]
                     last_time = item['_ts']
-                    print(f"backend={backend} last_time={last_time}")
-
+                    logging.info(f"Backend {backend} was not called in the last {int(time.time()) - last_time} seconds. Setting priority to 0")
                     if int(time.time()) - last_time > pushback_interval_seconds:
+                        logging.info(f"Setting priority to 0")
                         change_priority_dict[backend] = 0
                 else:
-                    print(f"No records found for backend={backend}")
+                    logging.info(f"No records found for backend={backend}")
             except azure.cosmos.exceptions.CosmosHttpResponseError as e:
-                print(f"Error querying Cosmos DB: {e}")
+                logging.info(f"Error querying Cosmos DB: {e}")
 
 
     change_priority(api_url, change_priority_dict)
@@ -162,6 +148,14 @@ def set_priorities(window_size_seconds=60, priority_step=50):
 
 
 if __name__ == "__main__":
+
+    # configure  the logging debugger
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+    # Set up a higher logging level specifically for Azure Cosmos DB to silence its logs
+    cosmos_logger = logging.getLogger('azure.cosmos')
+    cosmos_logger.setLevel(logging.WARNING)
+
+
     import matplotlib.pyplot as plt
     calls_number_plot = {}
     response_parameter_plot = {}
@@ -175,19 +169,18 @@ if __name__ == "__main__":
 
         # Plot new data
         for backend_url in response_parameter:
-            plt.plot(response_parameter[backend_url], label=f"{backend_url} - Response Time")
+            plt.plot(response_parameter[backend_url], label=f"{backend_url} - Response Time {change_priority_dict[backend_url[:-7]][-1]}")
         # Get the legend info for first plot
         handles1, labels1 = plt.gca().get_legend_handles_labels()
 
         ax2 = plt.twinx()  # Create a second y-axis
         for backend_url in calls_number:
-            ax2.plot(calls_number[backend_url], '--', label=f"{backend_url} - Calls Number")
+            ax2.plot(calls_number[backend_url], '--', label=f"{backend_url} - Calls Number {change_priority_dict[backend_url[:-7]][-1]}")
         # Get the legend info for second plot
         handles2, labels2 = ax2.get_legend_handles_labels()
 
         # plot the priorites in a separate scale in the same plot
         priorities = []
-        print(f"change_priority_dict={change_priority_dict}")
         for backend_url in response_parameter:
             if backend_url[:-7] in change_priority_dict:
                 priorities.append(change_priority_dict[backend_url[:-7]])
@@ -208,7 +201,6 @@ if __name__ == "__main__":
     with open("local.settings.json") as f:
         settings = json.load(f)
         for k, v in settings["Values"].items():
-            print(f"set {k}={v}")
             os.environ[k] = v
     cosmos_db_url = os.environ.get('COSMOS_DB_URL', 'https://openai-logger.documents.azure.com:443/')
     cosmos_db_key = os.environ.get('COSMOS_DB_KEY')
@@ -222,9 +214,6 @@ if __name__ == "__main__":
     
     while True:
         response_parameter, calls_number, change_priority_dict = set_priorities(window_size_seconds=60, priority_step=50)
-        print(f"response_parameter={response_parameter}")
-        print(f"calls_number={calls_number}")
-        print(f"change_priority_dict={change_priority_dict}")
         for k in calls_number.keys():
             if k not in calls_number_plot:
                 calls_number_plot[k] = []
