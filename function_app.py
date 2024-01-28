5eb372d1dbecfa73c132a5dab4749b3143843245
Your Name <amit>
2024-01-28 19:30:07 +0200

    changed to every minute

--- function_app.py
+++ function_app.py
@@ -4,7 +4,7 @@ from simple_priority_handler import set_priority
 import os
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 1 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False) 
+@app.schedule(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False) 
 def priority_job(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
